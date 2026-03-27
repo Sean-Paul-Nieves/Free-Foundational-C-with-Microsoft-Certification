e8f4e029fbe7af0576d37f4cd59de84a1930af92
Sean Nieves <paulnieves2417@gmail.com>
2026-03-27 16:32:16 +0800

    Exercise - Return values and parameters of methods

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -2,3 +2,13 @@
 // Random dice = new Random(); OR Random dice = new();
 // int roll = dice.Next(1,7);
 // Console.WriteLine(roll);
+
+// Exercise - Return values and parameters of methods
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
+
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");

5a966af46af28c4e8b31acd498bf5615347e493d
Sean Nieves <paulnieves2417@gmail.com>
2026-03-27 16:56:00 +0800

    Exercise - Create decision logic with if statements

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,21 +1,45 @@
-﻿// .NET Class Library exploration
+﻿#region .NET Class Library exploration
 // Random dice = new Random(); OR Random dice = new();
 // int roll = dice.Next(1,7);
 // Console.WriteLine(roll);
+#endregion
 
-// Exercise - Return values and parameters of methods
+#region Exercise - Return values and parameters of methods
 // Random dice = new Random();
 // int roll1 = dice.Next();
 // int roll2 = dice.Next(101);
 // int roll3 = dice.Next(50, 101);
-
 // Console.WriteLine($"First roll: {roll1}");
 // Console.WriteLine($"Second roll: {roll2}");
 // Console.WriteLine($"Third roll: {roll3}");
+#endregion
 
-// Exercise - Complete a challenge activity to discover and implement a method call
+#region Exercise - Complete a challenge activity to discover and implement a method call
 // int firstValue = 500;
 // int secondValue = 600;
 // int largerValue = Math.Max(firstValue, secondValue);
-
 // Console.WriteLine(largerValue);
+#endregion
+
+#region Exercise - Create decision logic with if statements
+Random dice = new Random();
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+if ((roll1 == roll2) && (roll2 == roll3)) 
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+if (total >= 15)
+    Console.WriteLine("You win!");
+if (total < 15)
+    Console.WriteLine("Sorry, you lose.");
+#endregion
